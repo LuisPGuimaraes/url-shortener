81c03673e193b0d241d7864454ebec0c19e9db88
LuisPGuimaraes <luisegsilva.le@gmail.com>
2026-01-11 19:22:08 -0300

    feat: implement unique short code generation and enhance short URL creation logic

--- src/url_shortener/service/short_url_service.clj
+++ src/url_shortener/service/short_url_service.clj
@@ -1,7 +1,31 @@
-(ns url-shortener.service.short-url-service)
+(ns url-shortener.service.short-url-service
+  (:require [url-shortener.repository.short-url-repository :as repo]
+            [url-shortener.schema.url-schema :as schema]))
+
+(def base-url "http://localhost:3000")
+
+(def chars "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+(defn generate-short-code
+  []
+  (let [code-length 6]
+    (->> (repeatedly code-length #(rand-nth chars))
+         (apply str))))
+
+(defn generate-unique-short-code
+  []
+  (loop [code (generate-short-code)]
+    (if (repo/find-by-short-url (str base-url "/" code))
+      (recur (generate-short-code))
+      code)))
 
 (defn create-short-url
-  [long-url]
-  {:short-code "abc123"
-   :original-url long-url
-   :short-url (str "http://localhost:3000/abc123")})
+  [original-url]
+  (if (repo/find-by-original-url original-url)
+    {:error true
+     :message "URL already exists in the database"}
+    (let [short-code (generate-unique-short-code)
+          short-url (str base-url "/" short-code)
+          document (schema/create-document short-url original-url)]
+      (repo/insert document)
+      document)))
