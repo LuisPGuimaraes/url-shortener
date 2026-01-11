75989da3a567d6f4cedfd56918d2561bd98f4dbb
LuisPGuimaraes <luisegsilva.le@gmail.com>
2026-01-11 20:23:25 -0300

    feat: enhance response handling in short URL controller and update redirect logic

--- src/url_shortener/controller/short_url_controller.clj
+++ src/url_shortener/controller/short_url_controller.clj
@@ -1,23 +1,26 @@
 (ns url-shortener.controller.short-url-controller
-  (:require [url-shortener.service.short-url-service :as short-url-service]))
+  (:require [url-shortener.service.short-url-service :as short-url-service]
+            [ring.util.response :as response]))
 
 (defn- success-response
+  "Creates a success response"
   [status body]
   {:status status
    :body body})
 
 (defn- error-response
+  "Creates an error response"
   [status message]
   {:status status
    :body {:error message}})
 
 (defn- redirect-response
+  "Creates a redirect response"
   [location]
-  {:status 302
-   :headers {"Location" location}
-   :body nil})
+  (response/redirect location))
 
 (defn create-short-url-handler
+  "Controller handler for creating a short URL"
   [request]
   (if-let [long-url (get-in request [:body-params :url])]
     (let [result (short-url-service/create-short-url long-url)]
@@ -27,9 +30,10 @@
     (error-response 400 "URL is required")))
 
 (defn redirect-handler
+  "Controller handler for redirecting to the original URL"
   [request]
   (if-let [short-code (get-in request [:path-params :short-code])]
-    (let [result (short-url-service/redirect-to-original-url short-code)]
+    (let [result (short-url-service/redirect-original-url short-code)]
       (if (:error result)
         (error-response 404 (:message result))
         (redirect-response (:original-url result))))
